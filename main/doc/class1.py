2a7ec93c9eff8fba69d9a17ed2437c431dc3a145
pythontutorials100 <systems.arw@gmail.com>
2025-03-25 08:23:31 -0700

    Update class1.py

--- main/doc/class1.py
+++ main/doc/class1.py
@@ -1,6 +1,9 @@
+import csv
+
+INPUT_CSV = "extended_defects.csv"  # or whatever your CSV file is named
+
 with open(INPUT_CSV, "r", encoding="utf-8") as f_in:
     reader = csv.DictReader(f_in)
     print("Detected headers:", reader.fieldnames)
     for row in reader:
         print(row)
-        # break  # just to see the first row
