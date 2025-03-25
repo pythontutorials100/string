41bb8b7371872ffde7b34300197c09bad7ee8860
pythontutorials100 <systems.arw@gmail.com>
2025-03-25 13:24:06 -0700

    Update class1.py

--- main/doc/class1.py
+++ main/doc/class1.py
@@ -1,17 +1,1 @@
-PREFIX :    <http://api.stardog.com/>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?defect ?defectID
-WHERE {
-  # 1) Find every individual typed as an Airfoil_Defect
-  ?defect rdf:type :Airfoil_Defect .
-  
-  # 2) Optionally match its “designative info” that carries a text ID
-  OPTIONAL {
-    ?defectICE rdf:type :Designative_Information_Content_Entity ;
-               :designates ?defect ;
-               :generically_depends_on ?defectIBE .
-    ?defectIBE :has_text_value ?defectID .
-  }
-}
-ORDER BY ?defect
