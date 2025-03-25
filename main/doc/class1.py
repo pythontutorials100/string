a1a1fa62cc00679aab22803d03d13d55b65dba5f
pythontutorials100 <systems.arw@gmail.com>
2025-03-25 08:20:21 -0700

    Update class1.py

--- main/doc/class1.py
+++ main/doc/class1.py
@@ -1,4 +1,6 @@
-number,edge/surface,region,inspect method,airfoil number,datum (1&2),Dmg. Edge Closest to Datum,Dmg. Edge Furthest to Datum,Dmg. Depth,Dmg. Length,fillet_radius,blend_depth,blend_length,flat_length,prop_factor,blend_loc,edge_node_comp
-1,edge,le,scanbox,1,tip,0.16,0.408,0.092,0.249,0,0.095,0.238,0.125,2,-0.285,LENODES
-2,edge,le,scanbox,1,tip,1.48,1.552,0.026,0.072,0,0.029,0.073,0,2,-1.516,LENODES
-3,edge,le,scanbox,3,tip,0.049,0.102,0.025,0.053,,,,,,,
+with open(INPUT_CSV, "r", encoding="utf-8") as f_in:
+    reader = csv.DictReader(f_in)
+    print("Detected headers:", reader.fieldnames)
+    for row in reader:
+        print(row)
+        # break  # just to see the first row
