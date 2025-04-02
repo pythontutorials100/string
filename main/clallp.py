22c336995361a2140720757a5543487f116f66fe
pythontutorials100 <systems.arw@gmail.com>
2025-04-02 02:11:40 -0700

    Update clallp.py

--- main/clallp.py
+++ main/clallp.py
@@ -1,1 +1,10 @@
+[MappingDeclaration] @collection [[
+mappingId     StudentMapping
+target        <http://example.org/student/{s_id}> a <http://example.org/voc/Student> ; <http://xmlns.com/foaf/0.1/firstName> "{first_name}"^^xsd:string ; <http://xmlns.com/foaf/0.1/lastName> "{last_name}"^^xsd:string .
+source        SELECT s_id, first_name, last_name FROM student
+]]
 
+[PrefixDeclaration]
+xsd:    <http://www.w3.org/2001/XMLSchema#>
+foaf:   <http://xmlns.com/foaf/0.1/>
+:       <http://example.org/voc/>
